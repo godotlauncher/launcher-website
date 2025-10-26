4d333fe6a85ffc262dde744e7da50f24a311c16b
Mario Debono <mario.debono@gmail.com>
2025-10-26 13:07:13 +0100

    feat: enhance HomepageHeader with clipboard copy functionality and styling

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,37 @@
+import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Heading from "@theme/Heading";
-import Link from "@docusaurus/Link";
 import clsx from "clsx";
 
-import styles from "./styles.module.css";
+import { useAgent } from "@site/src/hooks/useAgent";
+import { FC, useEffect, useState } from "react";
 import { HeroDownloadButton } from "../HeroDownloadButton";
-import { FC } from "react";
+import styles from "./styles.module.css";
+
+import { Check, Copy } from 'lucide-react';
 
 export const HomepageHeader: FC = () => {
   const { siteConfig } = useDocusaurusContext();
+  const { os } = useAgent();
+
+  const [swapped, setSwapped] = useState(false);
+
+  useEffect(() => {
+    let timeout: NodeJS.Timeout;
+    if (swapped) {
+      timeout = setTimeout(() => {
+        setSwapped(false);
+      }, 3000);
+    }
+    return () => {
+      if (timeout) clearTimeout(timeout);
+    };
+  }, [swapped]);
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText('winget install "Godot Launcher"');
+    setSwapped(true);
+  };
 
   return (
     <header className={clsx("hero shadow--lw", styles.heroBanner)}>
@@ -23,6 +46,27 @@ export const HomepageHeader: FC = () => {
           <HeroDownloadButton color="primary" />
         </div>
 
+        {os === "Windows" && (
+          <p className={clsx("margin-top--sm")}>
+            <div className="margin--md">Or run</div>
+            <center>
+              <code className={styles.codeBlock}>
+
+                <div className={styles.codeLabel}>Powershell & CMD</div>
+                <div className={styles.codeText}>
+                  &gt; winget install "Godot Launcher"
+                </div>
+                <button title="Copy to clipboard" className={styles.copyButton} onClick={handleCopy}>
+                  {!swapped ? <Copy width={16} /> : <Check color="#4caf50" width={16} />}
+                </button>
+
+              </code>
+            </center>
+
+
+          </p>
+        )}
+
         <p>
           Available for Windows, macOS, and Linux. Looking for{" "}
           <Link className={styles.heroLink} to="/download/archive">
@@ -31,6 +75,6 @@ export const HomepageHeader: FC = () => {
           ?
         </p>
       </div>
-    </header>
+    </header >
   );
 };
