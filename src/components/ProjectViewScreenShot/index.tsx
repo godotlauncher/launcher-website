b5dfe538a9b9de95a7a8a308a24d667a31f9f8ee
Mario Debono <mario.debono@gmail.com>
2026-03-07 13:36:35 +0100

    feat: add themed images to blogs, and pages.

--- src/components/ProjectViewScreenShot/index.tsx
+++ src/components/ProjectViewScreenShot/index.tsx
@@ -1,20 +1,17 @@
 import React from "react";
-import { useColorMode } from "@docusaurus/theme-common";
+import ThemedImage from "@theme/ThemedImage";
 import styles from "./styles.module.css";
 
 export const ProjectViewScreenShot: React.FC = () => {
-  const { colorMode } = useColorMode();
-  const screenshot = colorMode === "dark"
-    ? "/img/screenshots/screen_projects_view_dark.webp"
-    : "/img/screenshots/screen_projects_view_light.webp";
-
-
   return (
     <div className={styles.projectView}>
-      <img
+      <ThemedImage
         className={styles.projectView__image}
-        src={screenshot}
         alt="Screenshot of Godot Launcher"
+        sources={{
+          light: "/img/screenshots/screen_projects_view_light.webp",
+          dark: "/img/screenshots/screen_projects_view_dark.webp",
+        }}
       />
     </div>
   );
